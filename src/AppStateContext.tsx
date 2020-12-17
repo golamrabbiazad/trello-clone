ccd584d3d4fcd19c7e6e0392eb1ac1720cb35455
golamrabbiazad <golamrabbiazad@gmail.com>
2020-12-17 17:43:57 +0600

    small fn for id

--- src/AppStateContext.tsx
+++ src/AppStateContext.tsx
@@ -1,4 +1,6 @@
+import { nanoid } from 'nanoid';
 import React, { createContext, useReducer, useContext } from 'react';
+import { findItemIndexById, overrideItemAtIndex } from './utils/arrayUtils';
 
 interface Task {
   id: string;
@@ -37,6 +39,7 @@ const appData: AppState = {
 
 interface AppStateContextProps {
   state: AppState;
+  dispatch: React.Dispatch<Action>;
 }
 
 const AppStateContext = createContext<AppStateContextProps>(
@@ -44,8 +47,10 @@ const AppStateContext = createContext<AppStateContextProps>(
 );
 
 export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
+  const [state, dispatch] = useReducer(appStateReducer, appData);
+
   return (
-    <AppStateContext.Provider value={{ state: appData }}>
+    <AppStateContext.Provider value={{ state, dispatch }}>
       {children}
     </AppStateContext.Provider>
   );
@@ -53,4 +58,63 @@ export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
 
 export const useAppState = () => {
   return useContext(AppStateContext);
+};
+
+type Action =
+  | { type: 'ADD_LIST'; payload: string }
+  | {
+      type: 'ADD_TASK';
+      payload: {
+        text: string;
+        listId: string;
+      };
+    };
+
+const appStateReducer = (state: AppState, action: Action): AppState => {
+  switch (action.type) {
+    case 'ADD_LIST': {
+      return {
+        ...state,
+        lists: [
+          ...state.lists,
+          {
+            id: nanoid(),
+            text: action.payload,
+            tasks: [],
+          },
+        ],
+      };
+    }
+    case 'ADD_TASK': {
+      const targetListIndex = findItemIndexById(
+        state.lists,
+        action.payload.listId,
+      );
+
+      const targetList = state.lists[targetListIndex];
+
+      const updatedTargetList = {
+        ...targetList,
+        tasks: [
+          ...targetList.tasks,
+          {
+            id: nanoid(),
+            text: action.payload.text,
+          },
+        ],
+      };
+
+      return {
+        ...state,
+        lists: overrideItemAtIndex(
+          state.lists,
+          updatedTargetList,
+          targetListIndex,
+        ),
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 };
